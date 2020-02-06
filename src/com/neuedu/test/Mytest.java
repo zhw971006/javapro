066288b72572f37f36b0d655db7606c7cf61598f
zhw971006 <35446187+zhw971006@users.noreply.github.com>
2020-02-06 16:12:40 +0800

    提交项目

---
+++ src/com/neuedu/test/Mytest.java
@@ -0,0 +1,12 @@
+package com.neuedu.test;
+
+/**
+ * Created by Daniel on 2020/2/6.
+ */
+public class Mytest {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        System.out.println(a+b);
+    }
+}
